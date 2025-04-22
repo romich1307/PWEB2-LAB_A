498486ba3a3696dfe9f0193ceabc8bea6abbeed5
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 16:39:00 -0500

    arreglando indentacion

--- LAB3/Ejercicio4/script.js
+++ LAB3/Ejercicio4/script.js
@@ -2,12 +2,11 @@ function extraerCodigo() {
     const url = document.getElementById("urlInput").value.trim();
     const regex = /meet\.google\.com\/([a-zA-Z0-9\-]+)/;
     const match = url.match(regex);
-  
+
     if (match && match[1]) {
-      const codigo = match[1].replace(/-/g, ""); 
-      document.getElementById("resultado").textContent = `Código de la sesión: ${codigo}`;
+        const codigo = match[1].replace(/-/g, "");
+        document.getElementById("resultado").textContent = `Código de la sesión: ${codigo}`;
     } else {
-      document.getElementById("resultado").textContent = "URL inválida, por favor verifica.";
+        document.getElementById("resultado").textContent = "URL inválida, por favor verifica.";
     }
-  }
-  
+}
