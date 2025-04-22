426de07f4ac66ebf50e1ad3a0dcc9b0d24ce3648
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 15:11:17 -0500

    realizando primera funcion

---
+++ LAB3/Ejercicio1/dia_semana.js
@@ -0,0 +1,10 @@
+function obtenerDiaTexto(dia) {
+    const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    return dias[dia];
+  }
+  const hoy = new Date();
+  const numeroDia = hoy.getDay(); 
+  const textoDia = obtenerDiaTexto(numeroDia);
+  
+  console.log("Hoy es:", textoDia); 
+  
