3cfa985046b1988c12c6a830cf885ea3288feb8e
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 16:34:24 -0500

    arreglando indentacion

--- LAB3/Ejercicio2/script.js
+++ LAB3/Ejercicio2/script.js
@@ -2,5 +2,4 @@ function invertirTexto() {
     var texto = document.getElementById('texto').value;
     var textoInvertido = texto.split('').reverse().join('');
     document.getElementById('resultado').textContent = textoInvertido;
-  }
-  
+}
