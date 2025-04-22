03cb75dd26af04e515aa05d8bdb279151da2e0d3
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 16:32:57 -0500

    arreglando indentación

--- LAB3/Ejercicio1/dia_semana.js
+++ LAB3/Ejercicio1/dia_semana.js
@@ -1,10 +1,10 @@
 function obtenerDiaTexto(dia) {
     const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
     return dias[dia];
-  }
-  const hoy = new Date();
-  const numeroDia = hoy.getDay(); 
-  const textoDia = obtenerDiaTexto(numeroDia);
-  
-  console.log("Hoy es:", textoDia); 
-  
+}
+
+const hoy = new Date();
+const numeroDia = hoy.getDay();
+const textoDia = obtenerDiaTexto(numeroDia);
+
+console.log("Hoy es:", textoDia);
