bbd4cde4ec8dc345a1b099a1a6924e05dc4e7bf0
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 16:36:42 -0500

    arreglando indentacion

--- LAB3/Ejercicio3/script.js
+++ LAB3/Ejercicio3/script.js
@@ -2,17 +2,16 @@ function calcularDiasParaArequipa() {
     const hoy = new Date();
     const anioActual = hoy.getFullYear();
     const diaArequipa = new Date(anioActual, 7, 15);
-  
+
     if (hoy > diaArequipa) {
-      diaArequipa.setFullYear(anioActual + 1);
+        diaArequipa.setFullYear(anioActual + 1);
     }
-  
+
     const diferenciaTiempo = diaArequipa - hoy;
     const diasFaltantes = Math.ceil(diferenciaTiempo / (1000 * 60 * 60 * 24));
-  
+
     const mensaje = `Faltan ${diasFaltantes} día(s) para el Día de Arequipa`;
     document.getElementById('resultado').textContent = mensaje;
-  }
-  
-  calcularDiasParaArequipa();
-  
+}
+
+calcularDiasParaArequipa();
