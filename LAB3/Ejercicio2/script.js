d7284a13058f8d77f0a112afc73ff9c326bb409b
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-22 15:29:18 -0500

    creando funcion invertirTexto en script.js

---
+++ LAB3/Ejercicio2/script.js
@@ -0,0 +1,6 @@
+function invertirTexto() {
+    var texto = document.getElementById('texto').value;
+    var textoInvertido = texto.split('').reverse().join('');
+    document.getElementById('resultado').textContent = textoInvertido;
+  }
+  
