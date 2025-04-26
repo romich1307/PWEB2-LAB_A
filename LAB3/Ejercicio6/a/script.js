d9445aaf91b1fd0eea0f9971dc6c2a2277ccbd91
romich1307 <rcamargoh@unsa.edu.pe>
2025-04-25 20:01:28 -0500

    arreglando indentación

--- LAB3/Ejercicio6/a/script.js
+++ LAB3/Ejercicio6/a/script.js
@@ -2,16 +2,16 @@ let tamañoActual = 20;
 let colorActual = "black";
 
 function cambiarTamano() {
-  tamañoActual += 5;
-  if (tamañoActual > 200) tamañoActual = 16;
-  document.getElementById("texto").style.fontSize = tamañoActual + "px";
+    tamañoActual += 5;
+    if (tamañoActual > 200) tamañoActual = 16;
+    document.getElementById("texto").style.fontSize = tamañoActual + "px";
 }
 
 function cambiarColor() {
-  const colores = ["black", "blue", "red", "green", "purple"];
-  const indice = colores.indexOf(colorActual);
-  colorActual = colores[(indice + 1) % colores.length];
-  document.getElementById("texto").style.color = colorActual;
+    const colores = ["black", "blue", "red", "green", "purple"];
+    const indice = colores.indexOf(colorActual);
+    colorActual = colores[(indice + 1) % colores.length];
+    document.getElementById("texto").style.color = colorActual;
 }
 
 function actualizarTexto() {
