82f18d085989510b493d14d9afc12940e98179d1
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-01 14:32:50 -0500

    agrego ruta para servir index.html

--- LAB4/1_Actividades/ejercicio2/index.js
+++ LAB4/1_Actividades/ejercicio2/index.js
@@ -6,3 +6,6 @@ app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000");
 });
 
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'));
+});
