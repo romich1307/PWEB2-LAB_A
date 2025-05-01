36473c16b26787a5f1f7c4948a7cae2ae44adcc6
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-01 17:49:13 -0500

    pegando la funcion correcta en el app.js

--- LAB4/2_Ejercicios_Resueltos/Ejercicio2/pub/app.js
+++ LAB4/2_Ejercicios_Resueltos/Ejercicio2/pub/app.js
@@ -1,11 +1,11 @@
-function pedirPoema() {
-	fetch('/recitar')
-		.then(respuesta => respuesta.json())
-		.then(data => {
-			document.getElementById('resultado').innerHTML = data.text;
-		})
-		.catch(error => {
-			console.error("Error al pedir el poema:", error);
-			document.getElementById('resultado').textContent = "No se pudo cargar el poema.";
-		});
-}
+function recitar() {
+    const url = 'http://localhost:3000/recitar'
+    fetch(url).then(
+      response => response.json()
+    ).then(
+      data => {
+        document.querySelector("#poema").innerHTML = data.text
+      }
+    )
+  }
+  
