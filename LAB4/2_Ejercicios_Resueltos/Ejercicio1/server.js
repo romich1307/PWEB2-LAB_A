c4c6a47d7f504aa9ee06fa4644ab6441d158fc5c
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-01 16:57:18 -0500

    pego el contenido del server.js

--- LAB4/2_Ejercicios_Resueltos/Ejercicio1/server.js
+++ LAB4/2_Ejercicios_Resueltos/Ejercicio1/server.js
@@ -4,14 +4,10 @@ const app = express();
 
 app.use(express.static('pub'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pub', '1.html'));
-});
-
-app.get('/data.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'data.json'));
-});
-
 app.listen(3000, () => {
-  console.log("Servidor en: http://localhost:3000");
+	console.log("Escuchando en: http://localhost:3000");
+});
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'));
 });
