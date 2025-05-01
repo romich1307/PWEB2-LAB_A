2225661aae91485e4644fb1a59b9a3cfc80e8859
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-01 14:31:45 -0500

    configurando el servidor para escuchar en el puerto 3000

--- LAB4/1_Actividades/ejercicio2/index.js
+++ LAB4/1_Actividades/ejercicio2/index.js
@@ -2,3 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000");
+});
+
