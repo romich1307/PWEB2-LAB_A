3e08ca5dd0f380fbc1d0c5fe4400429c25170bdb
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-02 22:38:22 -0500

    copiando el contenido del script.js

---
+++ LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/public/Ejercicio2/script.js
@@ -0,0 +1,9 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "datos2.txt?t=" + Math.random());
+    xhttp.send();
+  }
+  
