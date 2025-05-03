4b18e0ab7cd016abe94b82bf52e9448948306207
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-02 22:40:24 -0500

    arreglando indent en el script.js

--- LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/public/Ejercicio2/script.js
+++ LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/public/Ejercicio2/script.js
@@ -1,9 +1,8 @@
 function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-    xhttp.open("GET", "datos2.txt?t=" + Math.random());
-    xhttp.send();
-  }
-  
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML = this.responseText;
+  };
+  xhttp.open("GET", "datos2.txt?t=" + Math.random());
+  xhttp.send();
+}
