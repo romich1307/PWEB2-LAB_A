1eee236b2ff4138953f3398d154cedc06a62485e
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-02 20:54:16 -0500

    copiamos el contenido script.js

---
+++ LAB4/3_Tarea_AJAX_w3Schools/AJAX XML Http/script.js
@@ -0,0 +1,12 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+  }
+  
