d686110a49758d6658146487411d8c6f07855916
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-02 23:30:49 -0500

    creando  contenido server.js

--- LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/server.js
+++ LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
+
+app.post('/datos4', (req, res) => {
+    const now = new Date().toLocaleString();
+    res.send(`This content was requested using the POST method.<br><br>Requested at: ${now}`);
+});
 
 app.listen(port, () => {
   console.log(`Servidor en http://localhost:${port}`);
