a6c33404780d51a413a92412a32516f4915ed573
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-04 15:44:03 -0500

    AREEGLANDO INDENTACION

--- LAB4/2_Ejercicios_Resueltos/Ejercicio3/pub/app.js
+++ LAB4/2_Ejercicios_Resueltos/Ejercicio3/pub/app.js
@@ -1,32 +1,31 @@
 function recitar(markupText) {
-    const url = 'http://localhost:3000/';  
-    const data = {
-      text: markupText  
-    };
-  
-    const request = {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json', 
-      },
-      body: JSON.stringify(data), 
-    };
-  
-    fetch(url, request)
-      .then(response => response.json())
-      .then(data => {
-        document.querySelector("#htmlCode").innerHTML = data.text; 
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    const text = document.querySelector('#markupText');
-    document.querySelector('#markupForm').onsubmit = () => {
-      recitar(text.value); 
-      return false;
-    };
-  });
-  
+  const url = 'http://localhost:3000/';
+  const data = {
+    text: markupText
+  };
+
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(url, request)
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector("#htmlCode").innerHTML = data.text;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const text = document.querySelector('#markupText');
+  document.querySelector('#markupForm').onsubmit = () => {
+    recitar(text.value);
+    return false;
+  };
+});
--- LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/public/Ejercicio4/script.js
+++ LAB4/3_Tarea_AJAX_w3Schools/solicitud AJAX/public/Ejercicio4/script.js
@@ -1,9 +1,8 @@
 function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-    xhttp.open("POST", "/datos4", true);
-    xhttp.send();
-  }
-  
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("demo").innerHTML = this.responseText;
+  };
+  xhttp.open("POST", "/datos4", true);
+  xhttp.send();
+}
