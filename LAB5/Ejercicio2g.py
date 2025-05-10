b81c1c8cf1678bd0e5bcb423e051a96210c5d5d7
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-10 15:58:13 -0500

    arreglando ejercicio 7 porque no mostraba bien las michas ni el tablero

--- LAB5/Ejercicio2g.py
+++ LAB5/Ejercicio2g.py
@@ -19,11 +19,12 @@ peones_blancos = pawn.horizontalRepeat(8)
 fila_blancas = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
 
 # Cuatro filas vacías
-vacio = square.horizontalRepeat(8)
-espacios = vacio.verticalRepeat(4)
+espacio1 = square.join(square.negative()).horizontalRepeat(4).verticalRepeat(1)
+espacio2 = square.negative().join(square).horizontalRepeat(4).verticalRepeat(1)
+
 
 # Unir las piezas en orden correcto: negras arriba, blancas abajo
-piezas = fila_negras.up(peones_blancos).up(espacios).up(peones_negros).up(fila_blancas)
+piezas = fila_blancas.up(peones_blancos).up(espacio2).up(espacio1).up(espacio2).up(espacio1).up(peones_negros).up(fila_negras)
 
 # Superponer las piezas sobre el tablero
 imagen = tablero.under(piezas)
