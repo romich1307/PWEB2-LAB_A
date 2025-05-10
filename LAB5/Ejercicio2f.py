39d3167c19fa84b20104f4656091aa26a84e025a
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-10 15:35:19 -0500

    Realizando el dibujo del ejercicio6

--- LAB5/Ejercicio2f.py
+++ LAB5/Ejercicio2f.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+
+fila1 = square.join(square.negative()).horizontalRepeat(4)
+fila2 = square.negative().join(square).horizontalRepeat(4)
+tablero = fila1.up(fila2).verticalRepeat(4)
+draw(tablero)
