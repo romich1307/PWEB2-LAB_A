194194028d3f8293080af895b3f2ac4081ebc9fd
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-10 15:37:23 -0500

    Arreglando el ejercicio6

--- LAB5/Ejercicio2f.py
+++ LAB5/Ejercicio2f.py
@@ -3,5 +3,5 @@ from chessPictures import *
 
 fila1 = square.join(square.negative()).horizontalRepeat(4)
 fila2 = square.negative().join(square).horizontalRepeat(4)
-tablero = fila1.up(fila2).verticalRepeat(4)
+tablero = fila2.up(fila1).verticalRepeat(2)
 draw(tablero)
