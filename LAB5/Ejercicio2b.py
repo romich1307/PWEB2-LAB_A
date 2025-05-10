4470a598555a2dd84e420366aa507be6a5dc7ef8
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-10 14:47:31 -0500

    Realizando el dibujo del segundo ejercicio

--- LAB5/Ejercicio2b.py
+++ LAB5/Ejercicio2b.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+fila1 = knight.join(knight.negative())
+fila2 = knight.negative().join(knight)
+imagen = fila1.up(fila2).verticalMirror()
+
+draw(imagen)
