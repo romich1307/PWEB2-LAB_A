61508e83389a2d53f70a2370b5be61562af45b0f
romich1307 <rcamargoh@unsa.edu.pe>
2025-05-11 13:34:03 -0500

    crenado un ejercicio extra para probar el metodo rotate

---
+++ LAB5/Ejercicio2h_extra.py
@@ -0,0 +1,8 @@
+from interpreter import draw
+from chessPictures import *
+
+# Rotamos la reina 90 grados en sentido horario
+imagen_rotada = queen.rotate()
+
+# Dibujamos la imagen rotada
+draw(imagen_rotada)
