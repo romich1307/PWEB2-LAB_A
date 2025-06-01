0976d16698e368e4cc8e0a02b717f34c73420a55
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-01 03:25:05 -0500

    Implementando todo lo de mencionado en el video en la parte de  "Passing message in Django"

--- LAB7/accounts/views.py
+++ LAB7/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 
@@ -15,15 +16,18 @@ def register(request):
         
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print("Username taken")
+                messages.info(request, "Username taken")
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print("Email taken")
+                messages.info(request, "Email taken")
+                return redirect('register')
             else:
                 user =User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print("User Created")    
         else:
-            print("Passwords do not match")
+            messages.info(request,"Passwords do not match")
+            return redirect('register')
         return redirect('/')
     
     else:
