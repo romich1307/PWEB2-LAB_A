6f206de5ce1fb84d31908718d664b4b4cb38707e
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-03 23:45:53 -0500

    copiando contenido en models.py de relacion_uno_muchos

--- LAB8/proyecto_relaciones/relacion_uno_muchos/models.py
+++ LAB8/proyecto_relaciones/relacion_uno_muchos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.language.name})"
