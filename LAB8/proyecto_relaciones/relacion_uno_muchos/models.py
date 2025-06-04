e069a26435d2d67b87bb66aac3488447f0acb42c
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-03 23:50:13 -0500

    añadiendo las clases en relacion_uno_mouchos models.py

--- LAB8/proyecto_relaciones/relacion_uno_muchos/models.py
+++ LAB8/proyecto_relaciones/relacion_uno_muchos/models.py
@@ -12,3 +12,16 @@ class Framework(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.language.name})"
+
+class DateExample(models.Model):
+    the_date = models.DateTimeField()
+
+class NullExample(models.Model):
+    col = models.CharField(max_length=10, blank=True, null=True)
+
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
