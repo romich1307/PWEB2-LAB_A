054c0884bbfa9f2ff71770ca1e93a6778f593dee
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-03 23:50:37 -0500

    registro los modelos en admin.py

--- LAB8/proyecto_relaciones/relacion_uno_muchos/admin.py
+++ LAB8/proyecto_relaciones/relacion_uno_muchos/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Language, Framework
+from .models import Simple, DateExample, NullExample, Language, Framework
 
+admin.site.register(Simple)
+admin.site.register(DateExample)
+admin.site.register(NullExample)
 admin.site.register(Language)
 admin.site.register(Framework)
