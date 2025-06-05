8bbbb4b48e85d63810957967ee6c73fc60da9d90
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-04 20:42:01 -0500

    replicando en models.py de relacion muchos a muchos

--- LAB8/proyecto_relaciones/relacion_muchos_muchos/models.py
+++ LAB8/proyecto_relaciones/relacion_muchos_muchos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
+
