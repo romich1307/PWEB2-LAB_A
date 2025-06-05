731ddf839152c1b9b92d2ec3f7cec57114afa400
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-04 19:34:53 -0500

    Añadiendo lo aprendido en el video numero 20 Query one to many

--- LAB8/proyecto_relaciones/relacion_uno_muchos/admin.py
+++ LAB8/proyecto_relaciones/relacion_uno_muchos/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Simple, DateExample, NullExample, Language, Framework
+from .models import Language, Framework
 
-admin.site.register(Simple)
-admin.site.register(DateExample)
-admin.site.register(NullExample)
 admin.site.register(Language)
 admin.site.register(Framework)
