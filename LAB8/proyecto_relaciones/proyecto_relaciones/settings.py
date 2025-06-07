23fca0df32d9d034bfcb1ee045c4d5ea691ddb3f
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-06 21:49:51 -0500

    cambiando mi base de datos por una postgres

--- LAB8/proyecto_relaciones/proyecto_relaciones/settings.py
+++ LAB8/proyecto_relaciones/proyecto_relaciones/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proyecto_relaciones.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proyecto_relaciones',
+        'USER': 'Romina',
+        'PASSWORD': 'laboratorio8',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
