63f3426e86db2966a96c5c70c843a3f81b0bd879
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-06 21:56:34 -0500

    aumentando el tamaño

--- LAB8/proyecto_relaciones/relacion_muchos_muchos/models.py
+++ LAB8/proyecto_relaciones/relacion_muchos_muchos/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Movie(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Character(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
     movies = models.ManyToManyField(Movie)
 
     def __str__(self):
