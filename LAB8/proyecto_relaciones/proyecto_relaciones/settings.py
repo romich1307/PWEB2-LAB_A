da04ccde706287d962096d8797bee842bd6234ab
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-06 22:58:54 -0500

    agregar carpeta templates

--- LAB8/proyecto_relaciones/proyecto_relaciones/settings.py
+++ LAB8/proyecto_relaciones/proyecto_relaciones/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'relacion_uno_muchos',
     'relacion_muchos_muchos',
+    'generar_pdf',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'proyecto_relaciones.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
