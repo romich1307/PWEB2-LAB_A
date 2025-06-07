44a4e21c68eab159767180260d8211e481ba3100
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-06 22:48:40 -0500

    añadiendo vistas en views.py

--- LAB8/proyecto_relaciones/relacion_muchos_muchos/views.py
+++ LAB8/proyecto_relaciones/relacion_muchos_muchos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from relacion_uno_muchos.models import Language, Framework
+from relacion_muchos_muchos.models import Movie, Character
 
-# Create your views here.
+def index(request):
+    languages = Language.objects.all()
+    frameworks = Framework.objects.all()
+    movies = Movie.objects.all()
+    characters = Character.objects.all()
+    return render(request, 'index.html', {
+        'languages': languages,
+        'frameworks': frameworks,
+        'movies': movies,
+        'characters': characters
+    })
